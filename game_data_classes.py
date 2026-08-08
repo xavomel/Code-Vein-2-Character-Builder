5d0e996b4b2f7e815ce525c01d41015961aebc0f
xavomel <xavomel@msn.com>
2026-08-08 19:41:50 +0200

    add loading game data from json and data class constructors

--- game_data_classes.py
+++ game_data_classes.py
@@ -1,5 +1,5 @@
 class Weapon:
-    name = "Weapon"
+    name = ""
     description = ""
     type = "" # One-Handed Swords, Two-Handed Swords, Twin Blades, Bayonets, Halberds, Hammers, Rune Blades
     bleed = 0
@@ -23,12 +23,24 @@ class Weapon:
         "Fortitude": 	0,
     }
 
-    def __init__(self, number):
-        self.name += " " + str(number)
+    def __init__(self, doc=None, dummy_number=None):
+        if not doc:
+            self.name += "Weapon " + str(dummy_number)
+            return
+
+        self.name = doc["Name"]
+        self.description = doc["Description"]
+        self.type = doc["Type"]
+        self.bleed = doc["Bleed"]
+        # self.scaling = "TODO"
+        self.transformable = doc["Transformable"]
+        self.transforms = {k: v for k, v in doc.items() if k.startswith("Transform_")}
+        self.capacity = doc["Capacity"]
+        self.burden = doc["Burden"]
 
 
 class Forma:
-    name = "Forma"
+    name = ""
     description = ""
     # One-Handed Swords, Two-Handed Swords, Twin Blades, Bayonets, Halberds, Hammers, Rune Blades
     # Magic Range Long, Magic Range Short, Assistance Attack, Assistance Defense, Assistance Other
@@ -42,12 +54,20 @@ class Forma:
         "Conductivity": 	0,
     }
 
-    def __init__(self, number):
-        self.name += " " + str(number)
+    def __init__(self, doc=None, dummy_number=None):
+        if not doc:
+            self.name = "Forma " + str(dummy_number)
+            return
+
+        self.name = doc["Name"]
+        self.description = doc["Description"]
+        self.type = doc["Type"]
+        self.ichor_cost = doc["IchorCost"]
+        self.capacity = doc["Capacity"]
 
 
 class Booster:
-    name = "Booster"
+    name = ""
     description = ""
     type = ""  # 0, 1, 2, 3
     conditions = []
@@ -71,12 +91,21 @@ class Booster:
         "Fortitude": 	0,
     }
 
-    def __init__(self, number):
-        self.name += " " + str(number)
+    def __init__(self, doc=None, dummy_number=None):
+        if not doc:
+            self.name = "Booster " + str(dummy_number)
+            return
+
+        self.name = doc["Name"]
+        self.description = doc["Description"]
+        self.type = doc["Type"]
+        # self.conditions = doc[""]
+        # self.attributes = doc["Attributes"]
+        self.burden = doc["Burden"]
 
 
 class BloodCode:
-    name = "Blood Code"
+    name = ""
     description = ""
     bloodline = ""
     bleed = 0
@@ -113,16 +142,33 @@ class BloodCode:
         "Lightning": 	0.0,
     }
 
-    resistances = {
+    resistance = {
         "Disease":  0,
         "Wound":    0,
         "Bleed":    0,
         "Curse":    0,
     }
 
+    def __init__(self, doc=None):
+        if not doc:
+            self.name = "Blood Code"
+            return
+
+        self.name = doc["Name"]
+        self.description = doc["Description"]
+        self.bloodline = doc["Bloodline"]
+        self.bleed = doc["Bleed"]
+        self.ichor = doc["Ichor"]
+        self.balance = doc["Balance"]
+        # self.traits = []
+        self.attributes = doc["Attributes"]
+        # self.burden = doc["Burden"]
+        self.defense = doc["Defense"]
+        self.resistance = doc["Resistance"]
+
 
 class Jail:
-    name = "Jail"
+    name = ""
     description = ""
     type = ""
     balance = 0
@@ -146,9 +192,21 @@ class Jail:
         "Lightning": 	0.0,
     }
 
+    def __init__(self, doc=None):
+        if not doc:
+            self.name = "Jail"
+            return
+
+        self.name = doc["Name"]
+        self.description = doc["Description"]
+        self.type = doc["Type"]
+        self.balance = doc["Balance"]
+        self.burden = doc["Burden"]
+        self.defense = doc["Defense"]
+
 
 class DefensiveForma:
-    name = "Defensive"
+    name = ""
     description = ""
     type = ""
     ichor_cost = 0
@@ -186,16 +244,56 @@ class DefensiveForma:
         "Lightning": 	0,
     }
 
-    resistances = {
+    resistance = {
         "Disease":  0,
         "Wound":    0,
         "Bleed":    0,
         "Curse":    0,
     }
 
+    def __init__(self, doc=None):
+        if not doc:
+            self.name = "Defensive"
+            return
+
+        self.name = doc["Name"]
+        self.description = doc["Description"]
+        self.type = doc["Type"]
+        self.ichor_cost = doc["IchorCost"]
+        self.stamina_guard_cost = doc["StaminaGuardCost"]
+        self.balance = doc["Balance"]
+        self.transformable = False
+        self.transforms = {k: v for k, v in doc.items() if k.startswith("Transform_")}
+        self.burden = doc["Burden"]
+        self.defense = doc["Defense"]
+        self.guarding_defense = doc["GuardingDefense"]
+        self.resistance = doc["Resistance"]
+
 
 class OffensiveForma:
-    name = "Offensive"
+    name = ""
     description = ""
     ichor_cost = 0
     scaling = {}
+
+    def __init__(self, doc=None):
+        if not doc:
+            self.name = "Offensive"
+            return
+
+        self.name = doc["Name"]
+        self.description = doc["Description"]
+        self.ichor_cost = doc["IchorCost"]
+        self.scaling = doc["Scaling"]
+
+
+def escape_filename(filename):
+    # replace : with 2 underscores
+    # replace " " with 1 underscore
+    return filename.replace(":", "__").replace(" ", "_")
+
+
+def unescape_filename(filename):
+    # replace 2 underscores with :
+    # replace underscore with " "
+    return filename.replace("__", ":").replace("_", " ")
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from PySide6.QtWidgets import QApplication, QMainWindow
+import json
 import builder_ui
 from game_data_classes import *
 
@@ -10,15 +11,15 @@ class Character:
     # need a separate variable for capacity weapon 1 and 2 ?
     # so that we can have max capacity 0 even without weapon
     # unless we use Weapon with default values for that
-    weapon_1 = Weapon(1)
-    weapon_2 = Weapon(2)
+    weapon_1 = Weapon(dummy_number=1)
+    weapon_2 = Weapon(dummy_number=2)
     offensive_forma = OffensiveForma()
     defensive_forma = DefensiveForma()
     jail = Jail()
 
-    formae_1 = [Forma(x) for x in range(4)]
-    formae_2 = [Forma(x) for x in range(4)]
-    boosters = [Booster(x) for x in range(6)]
+    formae_1 = [Forma(dummy_number=x + 1) for x in range(4)]
+    formae_2 = [Forma(dummy_number=x + 1) for x in range(4)]
+    boosters = [Booster(dummy_number=x + 1) for x in range(6)]
 
     traits = []
 
@@ -102,12 +103,70 @@ class Character:
     #         weapon_prev = self.weapon_2
 
 
+class Builder:
+    character = Character()
+    blood_codes = dict()
+    weapons = dict()
+    boosters = dict()
+    formae = dict()
+    offensive_formae = dict()
+    defensive_formae = dict()
+    jails = dict()
+
+    def __init__(self):
+        blood_codes = self.open_json(u"GameData/BloodCode.json")
+        weapons = self.open_json(u"GameData/Weapon.json")
+        boosters = self.open_json(u"GameData/Booster.json")
+        formae = self.open_json(u"GameData/Forma.json")
+        offensive_formae = self.open_json(u"GameData/Offensive.json")
+        defensive_formae = self.open_json(u"GameData/Defensive.json")
+        jails = self.open_json(u"GameData/Jail.json")
+
+        for doc in blood_codes:
+            name = doc["Name"]
+            self.blood_codes[name] = BloodCode(doc)
+            # print(vars(self.blood_codes[name]))
+
+        for doc in weapons:
+            name = doc["Name"]
+            self.weapons[name] = Weapon(doc)
+            # print(vars(self.weapons[name]))
+
+        for doc in boosters:
+            name = doc["Name"]
+            self.boosters[name] = Booster(doc)
+            # print(vars(self.boosters[name]))
+
+        for doc in formae:
+            name = doc["Name"]
+            self.formae[name] = Forma(doc)
+            # print(vars(self.formae[name]))
+
+        for doc in offensive_formae:
+            name = doc["Name"]
+            self.offensive_formae[name] = OffensiveForma(doc)
+            # print(vars(self.offensive_formae[name]))
+
+        for doc in defensive_formae:
+            name = doc["Name"]
+            self.defensive_formae[name] = DefensiveForma(doc)
+            # print(vars(self.defensive_formae[name]))
+
+        for doc in jails:
+            name = doc["Name"]
+            self.jails[name] = Jail(doc)
+            # print(vars(self.jails[name]))
+
+    def open_json(self, filepath):
+        with open(filepath, encoding='utf-8') as _data:
+            return json.load(_data)
+
+
 class MainWindow(QMainWindow, builder_ui.Ui_MainWindow):
     def __init__(self, parent=None):
         super(MainWindow, self).__init__(parent)
         self.setupUi(self)
-
-        self.character = Character()
+        self.builder = Builder()
 
 
 def main():
@@ -123,7 +182,6 @@ def main():
     # since there is no short unique identifier for items (e.g. weapons)
 
     # sorting order cannot be an unique identifier, because order may change in DLC
-
     app.exec()
     
 
